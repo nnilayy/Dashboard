866a8602a34fb8b5f93359e02a60fff20818c39b
unknown <nnilayy.work@gmail.com>
2024-11-25 15:27:17 +0530

    Push Recent Build

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <ThemedLayoutV2
       Header={Header}
-      Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Hello" />}
+      Title={(titleProps) => <ThemedTitleV2 {...titleProps} text="Customer Relations Management & Analytics Platform" />}
     >
       {children}
     </ThemedLayoutV2>
